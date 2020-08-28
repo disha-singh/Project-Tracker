151f78a7fa0a8ea6ecd96cad263d467f410a1330
Disha Singh <dishasingh@Dishas-MacBook-Air.local>
2020-08-28 21:43:07 +0530

    first commit

---
+++ app/src/main/java/com/example/projecttracker/Activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.projecttracker.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projecttracker.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
